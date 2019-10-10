ef7050bd4532e925fbe779662ab854e07b791376
Lauren Schaller <lauren.schaller@animoto.com>
2019-10-10 12:44:09 -0400

    set up endpoint

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const path = require('path')
+const moveTrain = require('./movetrain.js');
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use(bodyParser.urlencoded());
+
+app.post('/move-the-train', (req, res) => {
+  const stopNumber = req.body.stopNumber;
+
+  res.status(200).send({
+    success: 'true',
+    message: 'train moved to stop ' + stopNumber
+  })
+
+  moveTrain(stopNumber);
+});
---
+++ movetrain.js
@@ -0,0 +1,3 @@
+module.exports = function(stopNumber) {
+  console.log(stopNumber);
+}
